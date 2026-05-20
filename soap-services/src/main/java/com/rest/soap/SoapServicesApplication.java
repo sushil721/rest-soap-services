d730efa0574d9fb0824c8c47712000969f9c828d
sushil721 <singh.sushil721@gmail.com>
2026-05-20 20:33:03 +0530

    Initial commit

---
+++ soap-services/src/main/java/com/rest/soap/SoapServicesApplication.java
@@ -0,0 +1,13 @@
+package com.rest.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapServicesApplication.class, args);
+	}
+
+}
---
+++ soap-services/src/test/java/com/rest/soap/SoapServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
